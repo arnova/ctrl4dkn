ed4c13b2d1526332d50202e06eb46e848667152a
arnova <arnova77@gmail.com>
2024-11-07 10:52:12 +0100

    changed: Set room temperature average time to 30 minutes

--- include/daikinctrl.h
+++ include/daikinctrl.h
@@ -58,7 +58,7 @@
 #define LEAVING_WATER_MAX                    45.0f
 #define LEAVING_WATER_MAX_MARGIN              3.0f
 #define DAIKIN_ZONE_SWITCH_TIME                 5   // Seconds
-#define PRIMARY_ZONE_ROOM_TEMP_AVG_TIME       900   // Seconds = 15 minutes
+#define PRIMARY_ZONE_ROOM_TEMP_AVG_TIME      1800   // Seconds = 30 minutes
 #define PRIMARY_ZONE_VALVE_DELAY              180   // Seconds = 3 minutes
 #define FLOOR_PROTECTION_DELAY                300   // Seconds = 5 minutes
 
