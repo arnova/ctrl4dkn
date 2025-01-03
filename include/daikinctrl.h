6e7a489a15e183b64a755ee0944e330c51cd4947
arnova <arnova@void.org>
2025-01-03 10:39:09 +0100

    changed: Default to primary zone requires heating on startup

--- include/daikinctrl.h
+++ include/daikinctrl.h
@@ -143,7 +143,7 @@ class CDaikinCtrl
     PubSubClient* m_pMQTTClient;
     bool m_bDaikinPrimaryZoneOn = false;
     bool m_bDaikinSecondaryZoneOn = false;
-    bool m_bPrimaryZoneRequiresHeating = false;
+    bool m_bPrimaryZoneRequiresHeating = true;
     bool m_bPrimaryZoneValveClose = false;
     bool m_bFloorProtection = false;
     bool m_bDaikinActive = false;
