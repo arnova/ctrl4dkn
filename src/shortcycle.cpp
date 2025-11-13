c191db93d3e7ca59cb87f3594a8ac07b08431c42
arnova <arnova@eld>
2025-11-13 13:25:03 +0100

    fixed: Wrong braces

--- src/shortcycle.cpp
+++ src/shortcycle.cpp
@@ -29,11 +29,11 @@ void CShortCycle::Loop(const bool bOn)
   uint8_t iCycleCountSmall = 0;
   uint8_t iCycleCountLarge = 0;
   int8_t iTimeStampPos = 0;
-  uint32_t iTimeDiffOldest = (m_iShortCycleTimestamp - m_iShortCycleTimeStamps[0]) / (60 / CONTROL_LOOP_TIME);
+  uint32_t iTimeDiffOldest = (m_iShortCycleTimestamp - m_iShortCycleTimeStamps[0]) / 60 / CONTROL_LOOP_TIME;
   for (uint8_t it = 0; it < SHORT_CYCLE_SAMPLES; it++)
   {
     // Note: iTimeDiff already handles wrapping. Note that it's in minutes
-    const uint32_t iTimeDiff = (m_iShortCycleTimestamp - m_iShortCycleTimeStamps[it]) / (60 / CONTROL_LOOP_TIME);
+    const uint32_t iTimeDiff = (m_iShortCycleTimestamp - m_iShortCycleTimeStamps[it]) / 60 / CONTROL_LOOP_TIME;
 
     // Check if this is a short cycle in the small window
     if (iTimeDiff <= SHORT_CYCLE_SMALL_WINDOW)
